eb6e991fdafc30eaec6b9173d1ec45f0d0e0e1f1
colourgrey <120172454+chonggi-tokhu@users.noreply.github.com>
2025-05-31 13:59:26 +0900

    Update boost_audio_volume.js

--- boostvolume/boost_audio_volume.js
+++ boostvolume/boost_audio_volume.js
@@ -1,3 +1,107 @@
+var lamejs = window['lamejs'];
+function audioBufferToWav(aBuffer) {
+  let numOfChan = aBuffer.numberOfChannels,
+    btwLength = aBuffer.length * numOfChan * 2 + 44,
+    btwArrBuff = new ArrayBuffer(btwLength),
+    btwView = new DataView(btwArrBuff),
+    btwChnls = [],
+    btwIndex,
+    btwSample,
+    btwOffset = 0,
+    btwPos = 0;
+  setUint32(0x46464952); // "RIFF"
+  setUint32(btwLength - 8); // file length - 8
+  setUint32(0x45564157); // "WAVE"
+  setUint32(0x20746d66); // "fmt " chunk
+  setUint32(16); // length = 16
+  setUint16(1); // PCM (uncompressed)
+  setUint16(numOfChan);
+  setUint32(aBuffer.sampleRate);
+  setUint32(aBuffer.sampleRate * 2 * numOfChan); // avg. bytes/sec
+  setUint16(numOfChan * 2); // block-align
+  setUint16(16); // 16-bit
+  setUint32(0x61746164); // "data" - chunk
+  setUint32(btwLength - btwPos - 4); // chunk length
+
+  for (btwIndex = 0; btwIndex < aBuffer.numberOfChannels; btwIndex++)
+    btwChnls.push(aBuffer.getChannelData(btwIndex));
+
+  while (btwPos < btwLength) {
+    for (btwIndex = 0; btwIndex < numOfChan; btwIndex++) {
+      // interleave btwChnls
+      btwSample = Math.max(-1, Math.min(1, btwChnls[btwIndex][btwOffset])); // clamp
+      btwSample =
+        (0.5 + btwSample < 0 ? btwSample * 32768 : btwSample * 32767) | 0; // scale to 16-bit signed int
+      btwView.setInt16(btwPos, btwSample, true); // write 16-bit sample
+      btwPos += 2;
+    }
+    btwOffset++; // next source sample
+  }
+
+  let wavHdr = lamejs.WavHeader.readHeader(new DataView(btwArrBuff));
+
+  //Stereo
+  let data = new Int16Array(btwArrBuff, wavHdr.dataOffset, wavHdr.dataLen / 2);
+  let leftData = [];
+  let rightData = [];
+  for (let i = 0; i < data.length; i += 2) {
+    leftData.push(data[i]);
+    rightData.push(data[i + 1]);
+  }
+  var left = new Int16Array(leftData);
+  var right = new Int16Array(rightData);
+
+  if (AudioFormat === "MP3") {
+    //STEREO
+    if (wavHdr.channels === 2)
+      return wavToMp3Stereo(
+        wavHdr.channels,
+        wavHdr.sampleRate,
+        left,
+        right,
+      );
+    //MONO
+    else if (wavHdr.channels === 1)
+      return wavToMp3(wavHdr.channels, wavHdr.sampleRate, data);
+  } else return new Blob([btwArrBuff], { type: "audio/wav" });
+
+  function setUint16(data) {
+    btwView.setUint16(btwPos, data, true);
+    btwPos += 2;
+  }
+
+  function setUint32(data) {
+    btwView.setUint32(btwPos, data, true);
+    btwPos += 4;
+  }
+}
+
+function wavToMp3(channels, sampleRate, samples) {
+    var buffer = [];
+    var mp3enc = new lamejs.Mp3Encoder(channels, sampleRate, 128);
+    var remaining = samples.length;
+    var samplesPerFrame = 1152;
+    for (var i = 0; remaining >= samplesPerFrame; i += samplesPerFrame) {
+        var mono = samples.subarray(i, i + samplesPerFrame);
+        var mp3buf = mp3enc.encodeBuffer(mono);
+        if (mp3buf.length > 0) {
+            buffer.push(new Int8Array(mp3buf));
+        }
+        remaining -= samplesPerFrame;
+    }
+    var d = mp3enc.flush();
+    if(d.length > 0){
+        buffer.push(new Int8Array(d));
+    }
+
+    var mp3Blob = new Blob(buffer, {type: 'audio/mp3'});
+    var bUrl = window.URL.createObjectURL(mp3Blob);
+
+    // send the download link to the console
+    console.log('mp3 download:', bUrl);
+
+}
+
 /*var oldlog = console.log;
 console.log=function(){
     document.body.innerHTML+=arguments.join('')+'<br>';
@@ -165,6 +269,7 @@ var recordedel = null;
 var chunks = [];
 var duration = 0;
 var na = null;
+            var mymp3=null;
 async function startrecording() {
     //recordingstream = audioCtx.createMediaStreamDestination();
     /*recorder = new MediaRecorder(recordingstream.stream);
@@ -179,6 +284,18 @@ async function startrecording() {
         chunks.push(ev.data);
     });
     recorder.addEventListener('stop', (ev) => {
+        if (lamejs){
+            var reader = new FileReader();
+            var blob0=new Blob(chunks, { type: 'audio/wav' });
+            reader.readAsArrayBuffer(blob);
+            reader.onloadend=(ev)=>{
+                audioContext.decodeAudioData(reader.result,(audioBuffer)=>{
+                    mymp3 = audioBufferToWav(audioBuffer);
+                    recordedel.src=URL.createObjectURL(mymp3);
+                });
+            }
+            
+        }
         recordedel.src = URL.createObjectURL(new Blob(chunks, { type: 'audio/wav' }));
         recordedel.addEventListener("loadedmetadata", (ev) => {
             if (recordedel.duration === Infinity) {
