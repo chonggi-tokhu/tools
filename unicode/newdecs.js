4775b721c45240d7dee4274642e93a731aa3a51a
colourgrey <120172454+chonggi-tokhu@users.noreply.github.com>
2025-11-13 09:22:08 +0900

    .

---
+++ unicode/newdecs.js
@@ -0,0 +1,46 @@
+(function (gTh, ConvertDec) { "object" == typeof exports && "undefined" != typeof module ? module.exports = ConvertDec() : "function" == typeof define && define.amd ? define(ConvertDec) : (gTh = "undefined" != typeof globalThis ? globalThis : gTh || self).CGRConvertDec = ConvertDec() })(this, function () {
+
+            var systemstr='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            function toDec(code, charMap) {
+                if (typeof code !== 'string' || !(charMap instanceof Array)) {
+                    return 'ERROR: invalid parameter' | false;
+                }
+                var value = 0;
+                for (var i = 0; i < code.length; i++) {
+                    var chunk = code[i];
+                    value = value * charMap.length + (charMap.indexOf(chunk) < 0 ? 0 : charMap.indexOf(chunk));
+                }
+
+                return value;
+            }
+            function fromDec(code, charMap) {
+                if (!(charMap instanceof Array) || typeof code !== 'number' || isNaN(code)) {
+                    return 'ERROR: invalid parameter' | false;
+                }
+                var value = code;
+                var res = '';
+                var stop = 0;
+                while (value > 0) {
+                    var _0 = value % charMap.length;
+                    res = charMap[_0] + res;
+                    value = (value - _0) / charMap.length;
+                    stop++;
+                }
+                return res;
+            }
+            var converter = {
+                systemstr:'0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+                convert: function (codenumb = '', from, to, strp/*  = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ' */) {
+                    return fromDec(toDec(codenumb, (strp || this.systemstr).slice(0, from).split('')), (strp||this.systemstr).slice(0, to).split(''));
+                },
+                change_systemstr:function(strp){
+                    if (!(strp instanceof String ||typeof strp==='string')){
+                        return;
+                    }
+                    this.systemstr=strp;
+                }
+
+            };
+            return {converter:converter};
+        
+});
